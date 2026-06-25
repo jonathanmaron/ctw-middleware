7a6f9fb750db1706a74b529103019bd32b09a4f0
Jonathan Maron <jonathan.maron@theimagingsource.com>
2026-06-25 15:46:01 +0200

    i make MiddlewareTest final, document every test, and add xhtml + edge-case coverage

--- test/MiddlewareTest.php
+++ test/MiddlewareTest.php
@@ -4,15 +4,19 @@ declare(strict_types=1);
 namespace CtwTest\Middleware;
 
 use Ctw\Middleware\AbstractMiddleware;
+use DivisionByZeroError;
 use Middlewares\Utils\Dispatcher;
 use Psr\Http\Message\ResponseInterface as ResponseIface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use ReflectionClass;
 
-class MiddlewareTest extends AbstractCase
+final class MiddlewareTest extends AbstractCase
 {
-    public function testClassConstants(): void
+    /**
+     * Test that the class declares the HTML_SUFFIX and HTML_MIME_TYPES constants with the expected types.
+     */
+    public function testClassDeclaresHtmlSuffixAndHtmlMimeTypesConstants(): void
     {
         $reflectionClass = new ReflectionClass($this->getInstance());
 
@@ -25,7 +29,10 @@ class MiddlewareTest extends AbstractCase
         self::assertIsArray($constants['HTML_MIME_TYPES']);
     }
 
-    public function testContainsHtmlWithHtmlContentType(): void
+    /**
+     * Test that containsHtml() returns true when the Content-Type is text/html.
+     */
+    public function testContainsHtmlReturnsTrueWhenContentTypeIsTextHtml(): void
     {
         $stack    = [$middleware = $this->getInstance()];
         $response = Dispatcher::run($stack);
@@ -35,7 +42,23 @@ class MiddlewareTest extends AbstractCase
         self::assertTrue($middleware->publicContainsHtml($response));
     }
 
-    public function testContainsHtmlWithJsonContentType(): void
+    /**
+     * Test that containsHtml() returns true when the Content-Type is application/xhtml.
+     */
+    public function testContainsHtmlReturnsTrueWhenContentTypeIsApplicationXhtml(): void
+    {
+        $stack    = [$middleware = $this->getInstance()];
+        $response = Dispatcher::run($stack);
+        $response = $response->withHeader('Content-Type', 'application/xhtml');
+
+        // @phpstan-ignore-next-line
+        self::assertTrue($middleware->publicContainsHtml($response));
+    }
+
+    /**
+     * Test that containsHtml() returns false when the Content-Type is application/json.
+     */
+    public function testContainsHtmlReturnsFalseWhenContentTypeIsApplicationJson(): void
     {
         $stack    = [$middleware = $this->getInstance()];
         $response = Dispatcher::run($stack);
@@ -45,7 +68,10 @@ class MiddlewareTest extends AbstractCase
         self::assertFalse($middleware->publicContainsHtml($response));
     }
 
-    public function testContainsHtmlWithNoContentType(): void
+    /**
+     * Test that containsHtml() returns false when no Content-Type header is present.
+     */
+    public function testContainsHtmlReturnsFalseWhenContentTypeHeaderIsAbsent(): void
     {
         $stack    = [$middleware = $this->getInstance()];
         $response = Dispatcher::run($stack);
@@ -54,7 +80,10 @@ class MiddlewareTest extends AbstractCase
         self::assertFalse($middleware->publicContainsHtml($response));
     }
 
-    public function testGetSuffixStatistics(): void
+    /**
+     * Test that getSuffixStatistics() returns the input length, output length and savings percentage.
+     */
+    public function testGetSuffixStatisticsReturnsInputOutputLengthsAndSavingsPercentage(): void
     {
         $middleware = $this->getInstance();
 
@@ -75,6 +104,40 @@ class MiddlewareTest extends AbstractCase
         self::assertSame(70, $array[0]);
         self::assertSame(49, $array[1]);
         self::assertSame(30.0, $array[2]);
+    }
+
+    /**
+     * Test that getSuffixStatistics() reports a negative saving when the minified output is larger than the original.
+     */
+    public function testGetSuffixStatisticsReportsNegativeSavingWhenMinifiedIsLargerThanOriginal(): void
+    {
+        $middleware = $this->getInstance();
+
+        $original = '<p>x</p>';
+        $minified = '<p>xxxxx</p>';
+
+        // @phpstan-ignore-next-line
+        $array = $middleware->publicGetSuffixStatistics($original, $minified);
+        /** @var array{int, int, float} $array */
+
+        self::assertSame(8, $array[0]);
+        self::assertSame(12, $array[1]);
+        self::assertSame(-50.0, $array[2]);
+    }
+
+    /**
+     * Test that getSuffixStatistics() throws a DivisionByZeroError when the original is empty.
+     *
+     * @throws DivisionByZeroError When the original length is zero.
+     */
+    public function testGetSuffixStatisticsThrowsDivisionByZeroErrorWhenOriginalIsEmpty(): void
+    {
+        $middleware = $this->getInstance();
+
+        $this->expectException(DivisionByZeroError::class);
+
+        // @phpstan-ignore-next-line
+        $middleware->publicGetSuffixStatistics('', '');
     }
 
     private function getInstance(): AbstractMiddleware
